92c431e3794e4d65ae63ae6f1c3bfe648d346caf
Lin-Mao <linmaosdu@gmail.com>
2025-01-08 22:26:31 -0800

    Init commit

---
+++ pycuprof.cpp
@@ -0,0 +1,29 @@
+#include <pybind11/pybind11.h>
+#include "compute_sanitizer.h"
+
+static bool pycuprof_initialized = false;
+
+void cuprof_init() {
+    pycuprof_initialized = true;
+    enable_compute_sanitizer(false);
+}
+void cuprof_start() {
+    if (!pycuprof_initialized) {
+        throw std::runtime_error("CUProf is not initialized. Please call cuprof_init first.");
+    }
+    enable_compute_sanitizer(true);
+}
+void cuprof_stop() {
+    if (!pycuprof_initialized) {
+        throw std::runtime_error("CUProf is not initialized. Please call cuprof_init first.");
+    }
+    enable_compute_sanitizer(false);
+}
+
+PYBIND11_MODULE(pycuprof, m) {
+    m.doc() = "Python interface of CUProf";
+
+    m.def("cuprof_init", &cuprof_init, "Initialize CUProf (must be called at the very beginning)");
+    m.def("cuprof_start", &cuprof_start, "Start CUProf (start profiling, must be called after cuprof_init)");
+    m.def("cuprof_stop", &cuprof_stop, "Stop CUProf (stop profiling, must be called after cuprof_start)");
+}
